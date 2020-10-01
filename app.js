157285252e8fb9c348d517b64424703aebf38e67
Vlp90 <vladimir.leloup@gmail.com>
2020-10-01 18:04:00 +0200

    tracks in progress

--- app.js
+++ app.js
@@ -27,6 +27,15 @@ spotifyApi
   app.use(express.json());
 
 
+
+  // Handelbars
+
+  hbs.registerHelper("msToSeconds", function (millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  });
+  
 // setting the spotify-api goes here:
 
 // Our routes go here:
